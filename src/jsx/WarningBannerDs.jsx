ae7c44441de66dae50833a6257de970ba7664cd4
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2024-02-09 15:11:22 +0900

    fishing

--- src/jsx/WarningBannerDs.jsx
+++ src/jsx/WarningBannerDs.jsx
@@ -14,7 +14,7 @@ class WebPage extends React.Component {
                 <div>
                     <div id="section-left">
                         <div>
-                            <img src={process.env.PUBLIC_URL + '/caution.png'} alt="loading" width="25%" height="25%" style={{float: 'left'}} className="margin"/>
+                            <img src={process.env.PUBLIC_URL + '/res/caution.png'} alt="loading" width="25%" height="25%" style={{float: 'left'}} className="margin"/>
                             <article className="script">
                                 <p style={{height:'100%', width: '100%'}}>방금 전 귀하의 IP주소에서 <span className="underscore">불법•유해사이트로의 접근 시도</span>가 확인되었습니다. 즉시 해당 IP주소의 신고가 KICS(대한민국정보통신보안국)에 접수되었음을 알립니다. 이하 법률에 대한 위반사항이 있다면 해당 행위는 위법 행위입니다.</p>
                                 <hr style={{width:'100%', color:'#c0c0c0', clear: 'left'}}/>
@@ -23,7 +23,7 @@ class WebPage extends React.Component {
                         </div>
                     </div>
                     <div id="section-right">
-                        <img src={process.env.PUBLIC_URL + '/map-example.png'} alt="loading" width="90%" height="50%" className="margin"/>
+                        <img src={process.env.PUBLIC_URL + '/res/map-example.png'} alt="loading" width="90%" height="50%" className="margin"/>
                         <div className="margin" style={{color: '#ff0000', fontSize: '2vw'}}>
                             <p style={{color: '#ff0000'}}>Your IP:<br/>Your Address:</p>
                         </div>
